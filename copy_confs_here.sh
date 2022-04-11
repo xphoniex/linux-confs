e4e8163744e1c0bfaaa26b4641c7bb6f28a18c22
xphoniex <dj.2dixx@gmail.com>
2022-04-11 06:03:06 +0000

    cp fish_user_key_bindings.fish

--- copy_confs_here.sh
+++ copy_confs_here.sh
@@ -7,6 +7,7 @@ cp ~/.config/bspwm/bspwmrc .config/bspwm/
 cp ~/.config/polybar/config .config/polybar/config
 
 cp ~/.config/fish/config.fish .config/fish/
+cp ~/.config/fish/functions/fish_user_key_bindings.fish .config/fish/functions/
 cp ~/.alacritty.yml .
 
 cp /usr/lib/systemd/user/mpd.service usr/lib/systemd/user/mpd.service
