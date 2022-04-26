30db2af3529840a410403d92e6e4319573fc0fc7
xphoniex <dj.2dixx@gmail.com>
2022-04-26 06:03:07 +0000

    add mimeapps.list

--- copy_confs_here.sh
+++ copy_confs_here.sh
@@ -8,6 +8,8 @@ cp ~/.config/polybar/config .config/polybar/config
 
 cp ~/.config/fish/config.fish .config/fish/
 cp ~/.config/fish/functions/fish_user_key_bindings.fish .config/fish/functions/
+cp ~/.config/mimeapps.list .config/
+
 cp ~/.alacritty.yml .
 
 cp /usr/lib/systemd/user/mpd.service usr/lib/systemd/user/mpd.service
