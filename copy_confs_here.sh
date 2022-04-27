4a12b524955b94e7967c30d82348a3deeb44e4e8
xphoniex <dj.2dixx@gmail.com>
2022-04-27 06:03:07 +0000

    cp vscode config

--- copy_confs_here.sh
+++ copy_confs_here.sh
@@ -9,6 +9,7 @@ cp ~/.config/polybar/config .config/polybar/config
 cp ~/.config/fish/config.fish .config/fish/
 cp ~/.config/fish/functions/fish_user_key_bindings.fish .config/fish/functions/
 cp ~/.config/mimeapps.list .config/
+cp ~/.config/Code/User/settings.json .config/Code/User/
 
 cp ~/.alacritty.yml .
 
