f37b39703df7a2b05a6cd3f33dceb8d53c5bf252
xphoniex <xphoniex@users.noreply.github.com>
2025-01-08 06:29:38 +0000

    GM: update transcript downloader (use mutations)

--- greasemonkey/yt-download-transcripts.js
+++ greasemonkey/yt-download-transcripts.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Download YouTube Transcripts
-// @version  1.1
+// @version  1.2
 // @match    https://www.youtube.com/watch*
 // @author   xphoniex
 // @run-at   document-idle
@@ -12,79 +12,86 @@ let manual = false; // added 2024-12-01
 
 const videoId = document.location.toString().match(/v=(.{11})/)[1];
 
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (!clicked) {
-      for (const el of document.getElementsByClassName('yt-spec-button-shape-next yt-spec-button-shape-next--outline yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m')) {
-        if (el.ariaLabel == "Show transcript") {
-          el.click();
-          clicked = true;
+let mutationsArray = [];
+let observerDebounceTimer;
+
+function debounceObserver(mutations) {
+  clearTimeout(observerDebounceTimer);
+  mutationsArray.push(mutations);
+  observerDebounceTimer = setTimeout(dealWithMutations, 10);
+}
+
+let showTranscriptBtn = document.getElementsByClassName('yt-spec-button-shape-next yt-spec-button-shape-next--outline yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m')[0] || undefined;
+if (showTranscriptBtn) {
+  showTranscriptBtn.click();
+  clicked = true;
+}
+
+function dealWithMutations() {
+  const mutations = mutationsArray.flat();
+  mutationsArray = [];
+  const addedNodes = mutations.map((mutation) => mutation.addedNodes).filter((addedNodes) => addedNodes);
+
+  if (!clicked) {
+    addedNodes.map((nodeList) => Array.from(nodeList)).flat().map((node) => {
+      if (node.nodeName === 'BUTTON' && node.className === 'yt-spec-button-shape-next yt-spec-button-shape-next--outline yt-spec-button-shape-next--call-to-action yt-spec-button-shape-next--size-m') {
+        node.click();
+        clicked = true;
+      }
+    });
+  }
+
+  if (clicked && !archived) {
+    for (const el of document.getElementsByTagName('ytd-engagement-panel-section-list-renderer')) {
+      if (el.attributes["target-id"] // added 2024-10-18
+          && el.attributes["target-id"].value == "engagement-panel-searchable-transcript"
+          && el.attributes["visibility"].value == "ENGAGEMENT_PANEL_VISIBILITY_EXPANDED"
+          && el.innerText != "Transcript"
+          && el.innerText != "In this video\nTimeline\nChapters\nTranscript" // added 2024-10-18
+         )
+      {
+        // added 2024-12-01
+        const langNode = el.children[1].children[0].children[1].children[0].children[4];
+        if (!manual && langNode.innerText.substr(0,7) != "English") {
+          const node = document.createElement("a");
+          const textnode = document.createTextNode("Download");
+          node.appendChild(textnode);
+          node.onclick = () => { saveTranscript(); };
+          
+          el.children[0].appendChild(node);
+          manual = true;
+
+          // can return and leave transcript box open, and wait for manual intervention
+          // or
+          // can skip these two and download whatever transcript is available, but
+          // notify the user too
+          //
+          //archived = true;
+          //return;
+          
+          alert('YouTube transcription is not English, but ' + langNode.innerText + '.');
+        } else if (langNode.innerText != "English (auto-generated)") {
+          alert('YouTube transcription is not auto-generated English.');
+        }
+
+        saveTextAsFile(el.innerText, videoId + ".yt.txt", "");
+
+        archived = true;
+
+        // close it
+        for (const el of document.getElementsByClassName('yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-only-default')) {
+          if (el.ariaLabel == "Close transcript") {
+            el.click();
+            observer.disconnect();
+          }
         }
       }
     }
+  }
+}
 
-    if (clicked && !archived) {    
-      for (const el of document.getElementsByTagName('ytd-engagement-panel-section-list-renderer')) {
-        if (el.attributes["target-id"] // added 2024-10-18
-            && el.attributes["target-id"].value == "engagement-panel-searchable-transcript"
-            && el.attributes["visibility"].value == "ENGAGEMENT_PANEL_VISIBILITY_EXPANDED"
-            && el.innerText != "Transcript"
-            && el.innerText != "In this video\nTimeline\nChapters\nTranscript" // added 2024-10-18
-           ) 
-        {
-          // added 2024-12-01
-          const langNode = el.children[1].children[0].children[1].children[0].children[4];
-          if (!manual && langNode.innerText.substr(0,7) != "English") {           
-            const node = document.createElement("a");
-						const textnode = document.createTextNode("Download");
-            node.appendChild(textnode);
-            node.onclick = () => { archived = false; };
-            
-						el.children[0].appendChild(node);
-            manual = true;
-            
-            // can return and leave transcript box open, and wait for manual intervention
-            // or
-            // can skip these two and download whatever transcript is available, but 
-            // notify the user too
-            //
-            //archived = true;
-            //return;
-            
-            alert('YouTube transcription is not English, but ' + langNode.innerText + '.');
-          } else if (langNode.innerText != "English (auto-generated)") {
-            alert('YouTube transcription is not auto-generated English.');            
-          }
-          
-          saveTextAsFile(el.innerText, videoId + ".yt.txt", "");
-        
-        	archived = true;
-          
-          // close it
-          for (const el of document.getElementsByClassName('yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-only-default')) {
-            if (el.ariaLabel == "Close transcript") {
-              el.click();
-              window.scrollTo({ top: 0, behavior: 'smooth' });
-            }
-          }
-        }
-    	}
-    }
-    
-    
-    //for (const el of document.getElementsByClassName('ytp-caption-segment')) {
-    //  GM.xmlHttpRequest({
-    //    method: "POST",
-    //    url: "http://localhost:10111/" + videoId,
-    //    data: el.innerText,
-    //  });
-    //}
-
-  });
-});
-
-const config = {childList:true,subtree:true};
-observer.observe(document.body, config);
+const observer = new MutationObserver(debounceObserver);
+observer.observe(document.body, { childList: true, subtree: true });
 
 function saveTextAsFile(textToWrite, fileNameToSaveAs, fileType) {
     let textFileAsBlob = new Blob([textToWrite], { type: fileType });
@@ -94,4 +101,25 @@ function saveTextAsFile(textToWrite, fileNameToSaveAs, fileType) {
     downloadLink.style.display = 'none';
     document.body.appendChild(downloadLink);
     downloadLink.click();
+}
+
+function saveTranscript() {
+  for (const el of document.getElementsByTagName('ytd-engagement-panel-section-list-renderer')) {
+    if (el.attributes["target-id"] // added 2024-10-18
+        && el.attributes["target-id"].value == "engagement-panel-searchable-transcript"
+        && el.attributes["visibility"].value == "ENGAGEMENT_PANEL_VISIBILITY_EXPANDED"
+        && el.innerText != "Transcript"
+        && el.innerText != "In this video\nTimeline\nChapters\nTranscript" // added 2024-10-18
+       )
+    {
+      saveTextAsFile(el.innerText, videoId + ".yt.txt", "");
+
+      // close it
+      for (const el of document.getElementsByClassName('yt-spec-button-shape-next yt-spec-button-shape-next--text yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-only-default')) {
+        if (el.ariaLabel == "Close transcript") {
+          el.click();
+        }
+      }
+    }
+  }
 }
